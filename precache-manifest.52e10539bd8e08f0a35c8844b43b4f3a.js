b318116d9d406a656d548c8c32d3993dac2a6a64
vladislav kirilov <vladislavkirilov@192.168.1.44>
2020-08-24 22:14:48 +0300

    Updates

---
+++ precache-manifest.52e10539bd8e08f0a35c8844b43b4f3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfed1a49f175de1d1e2accc4201789d1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "40bcef2a05bdee56729e",
+    "url": "/portfolio/static/css/main.b7ef35a5.chunk.css"
+  },
+  {
+    "revision": "313472209a75ca48b69e",
+    "url": "/portfolio/static/js/2.0dadc523.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0dadc523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40bcef2a05bdee56729e",
+    "url": "/portfolio/static/js/main.8573ea01.chunk.js"
+  },
+  {
+    "revision": "70c8eb5229cac6b9859f",
+    "url": "/portfolio/static/js/runtime-main.a0190270.js"
+  },
+  {
+    "revision": "7869e84bdda526c2724a8d16fb6af33d",
+    "url": "/portfolio/static/media/preloader.7869e84b.svg"
+  }
+]);
