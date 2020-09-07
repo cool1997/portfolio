f6f4a3175f10eb698b08ff883cc69f5f4f8d60c7
cool1997 <palefire@gmail.com>
2020-09-07 15:54:05 +0300

    тест

--- build/precache-manifest.3691167285f2c20f15532e481dcdfda8.js
+++ build/precache-manifest.3691167285f2c20f15532e481dcdfda8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ec2b74d673ab474b43ec37a02a8e506",
+    "revision": "8b5ceeb6168e4a1d84103a385b392558",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "315ed395b0a8e2898a6f",
-    "url": "/portfolio/static/css/main.5e65817a.chunk.css"
+    "revision": "9652650e2c0c0fc1ba4b",
+    "url": "/portfolio/static/css/main.7ecb6385.chunk.css"
   },
   {
-    "revision": "f67963044cab50757f3c",
-    "url": "/portfolio/static/js/2.089dd764.chunk.js"
+    "revision": "89f90adddfcc1440fdc3",
+    "url": "/portfolio/static/js/2.3843dadb.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio/static/js/2.089dd764.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.3843dadb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "315ed395b0a8e2898a6f",
-    "url": "/portfolio/static/js/main.6bc31dae.chunk.js"
+    "revision": "9652650e2c0c0fc1ba4b",
+    "url": "/portfolio/static/js/main.27009fd7.chunk.js"
   },
   {
     "revision": "70c8eb5229cac6b9859f",
