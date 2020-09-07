4e9d1d6a306e866e404ab1df1f426e31fc461149
cool1997 <palefire@gmail.com>
2020-09-07 15:12:16 +0300

    Updates

---
+++ precache-manifest.f41735c5f5b87bd060b5234632d9ea6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec2b74d673ab474b43ec37a02a8e506",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "315ed395b0a8e2898a6f",
+    "url": "/portfolio/static/css/main.5e65817a.chunk.css"
+  },
+  {
+    "revision": "f67963044cab50757f3c",
+    "url": "/portfolio/static/js/2.089dd764.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.089dd764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315ed395b0a8e2898a6f",
+    "url": "/portfolio/static/js/main.6bc31dae.chunk.js"
+  },
+  {
+    "revision": "70c8eb5229cac6b9859f",
+    "url": "/portfolio/static/js/runtime-main.a0190270.js"
+  },
+  {
+    "revision": "88596d713afccacd6cf46a27ef858601",
+    "url": "/portfolio/static/media/preloader.88596d71.svg"
+  }
+]);
