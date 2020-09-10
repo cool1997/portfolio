d04eebba05ab37e993d501b953042eb909d03f44
cool1997 <palefire@gmail.com>
2020-09-10 14:50:44 +0300

    добавил стили туду айтемам

--- build/precache-manifest.908bdb96612fc139f825a9d53301ab68.js
+++ build/precache-manifest.908bdb96612fc139f825a9d53301ab68.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eaf1b6d5d68b57937aedce73442c4463",
+    "revision": "0a87b8ac682e9c741e1ac6550281e336",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "f7a0994df56eb2e0025f",
+    "revision": "1cd4faae451dde407216",
     "url": "/portfolio/static/css/main.bfd2d38f.chunk.css"
   },
   {
-    "revision": "b3ee90a748b15ae1cf8e",
-    "url": "/portfolio/static/js/2.cea972bb.chunk.js"
+    "revision": "25752964f42c128ad247",
+    "url": "/portfolio/static/js/2.808424a6.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio/static/js/2.cea972bb.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.808424a6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f7a0994df56eb2e0025f",
-    "url": "/portfolio/static/js/main.6cc1e1d9.chunk.js"
+    "revision": "1cd4faae451dde407216",
+    "url": "/portfolio/static/js/main.bfd3b6a8.chunk.js"
   },
   {
     "revision": "70c8eb5229cac6b9859f",
